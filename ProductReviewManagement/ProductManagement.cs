aa58c99abce686cded6d7541e9322960253bbd45
Siddhant <santoshshrivastava39@gmail.com>
2022-07-31 10:50:21 +0530

    Retrieve All records from list who's rating are greater than 3 and productID is 1 or 4 or 9

--- ProductReviewManagement/ProductManagement.cs
+++ ProductReviewManagement/ProductManagement.cs
@@ -8,16 +8,18 @@ namespace ProductReviewManagement
 {
     public class ProductManagement
     {
-        public static void TopThreeRecords(List<ProductReview> productReviewList)
+        public static void GreaterThanThreeRecords(List<ProductReview> productreviewlist)
         {
-            var productData = (from ProductReview in productReviewList orderby ProductReview.Rating descending select ProductReview).Take(3);
+            var productData = (from productReviews in productreviewlist
+                               where (productReviews.ProductID == 1 || productReviews.ProductID == 4 || productReviews.ProductID == 9)
+                               && productReviews.Rating > 3
+                               select productReviews);
 
             foreach (var list in productData)
             {
                 Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
                      + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "IsLike:- " + list.isLike);
             }
-
         }
 
 
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -23,7 +23,7 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductID = 5, UserID = 11, Rating = 4, Review = "Bad", isLike = true },
                 new ProductReview() { ProductID = 8, UserID = 1, Rating = 2, Review = "Good", isLike = false },
                 new ProductReview() { ProductID = 6, UserID = 2, Rating = 3, Review = "Nice", isLike = true },
-                new ProductReview() { ProductID = 3, UserID = 4, Rating = 5, Review = "Good", isLike = false },
+                new ProductReview() { ProductID = 9, UserID = 4, Rating = 5, Review = "Good", isLike = false },
                 new ProductReview() { ProductID = 3, UserID = 3, Rating = 5, Review = "Nice", isLike = true },
                 new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductID = 22, UserID = 3, Rating = 5, Review = "Good", isLike = true },
@@ -38,15 +38,14 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductID = 24, UserID = 6, Rating = 5, Review = "Good", isLike = false },
             };
 
-            foreach (var list in productreview)
-            {
-                Console.WriteLine("ProductId:" + list.ProductID + " " + "UserID:" + list.UserID + " " + "Rating:" + list.Rating + " " + "Review:" + list.Review + " " + "isLike:" + list.isLike);
-            }
+            //foreach (var list in productreview)
+            //{
+            //    Console.WriteLine("ProductId:" + list.ProductID + " " + "UserID:" + list.UserID + " " + "Rating:" + list.Rating + " " + "Review:" + list.Review + " " + "isLike:" + list.isLike);
+            //}
 
-            Console.WriteLine("---------------------------");
-            //Gives Top 3 Records whose ratings are higher
-            Console.WriteLine("Records which are having higher ratings are- ");
-            ProductManagement.TopThreeRecords(productreview);
+          
+            Console.WriteLine("Records whose ratings are greater then 3 and product Id is 1 or 4 or 9 are- ");
+            ProductManagement.GreaterThanThreeRecords(productreview);
         }
     }
 }
